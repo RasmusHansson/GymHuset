fd7f5e928e1f84e6a8bc45716d7d0cb3b246bf18
anderstal <oskaranderstal@gmail.com>
2014-03-03 16:55:48 +0100

    Produktcontroller
    inkl. views

--- GymHuset/GymHuset/Controllers/ProdukterController.cs
+++ GymHuset/GymHuset/Controllers/ProdukterController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,6 +16,24 @@ namespace GymHuset.Controllers
         {
             return View();
         }
+
+        public ActionResult Klader(string id)
+        {
+            return View();
+        }
+
+        public ActionResult Kosttillskott(string id)
+        {
+            return View();
+        }
+
+        public ActionResult Search(string searchString)
+        {
+            return View();
+        }
+
+     
+     
 
     }
 }
