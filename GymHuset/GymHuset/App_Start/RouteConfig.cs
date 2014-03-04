927569b7a8743a4aed48504602ec68cd482c607d
JamesRecond <c.k.johansson@gmail.com>
2014-03-04 15:52:23 +0100

    Revert "CSSBeta"
    This reverts commit 89a34a743142ebb30b602a4e7e62f32cc260cab2.

--- GymHuset/GymHuset/App_Start/RouteConfig.cs
+++ GymHuset/GymHuset/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace GymHuset
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "TestCSS", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Produkter", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
