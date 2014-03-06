cc04612ba9b6555da560fed448b237f8a408b275
RasmusHansson <rasmus89h@gmail.com>
2014-03-06 15:16:47 +0100

    Kommentarer till kod

--- GymHuset/GymHuset/Controllers/LayoutController.cs
+++ GymHuset/GymHuset/Controllers/LayoutController.cs
@@ -13,14 +13,17 @@ namespace GymHuset.Controllers
         // GET: /Layout/
         DataClasses1DataContext db = new DataClasses1DataContext();
 
+
         public ActionResult _PopProductsPartial()
         {
+            //Välj ut alla produkter i databasen och sortera på antal sålda produkter, flest -> minst
             List<tbProduct> productList = (from p in db.tbProducts
                                            select p).OrderByDescending(p => p.iItemsSold).ToList();
 
 
             List<GymHuset.Models.tbProduct> popularList = new List<GymHuset.Models.tbProduct>();
 
+            //Plocka ut de fem högst säljande produkterna och lägg i en ny lista 
             for (int i = 0; i < 5; i++)
             {
                 popularList.Add((productList).First());
@@ -28,6 +31,7 @@ namespace GymHuset.Controllers
                 productList.Remove((productList).First());
             }
 
+            //Skicka med listan på top 5 populära produkter till vyn Shared/_PopProductsPartial.cshtml
             return PartialView(popularList);
         }
 
